7a584d2b521192f333b338a8aa5eb8e99174eb53
EAOE3 <haddadedy82@gmail.com>
2023-12-05 13:51:48 +0400

    New Txn

--- src/main/java/com/github/pwrlabs/pwrj/protocol/PWRJ.java
+++ src/main/java/com/github/pwrlabs/pwrj/protocol/PWRJ.java
@@ -294,6 +294,25 @@ public class PWRJ {
     }
 
 
+    public static long getActiveVotingPower() {
+        try {
+            HttpGet request = new HttpGet(rpcNodeUrl + "/activeVotingPower/");
+            HttpResponse response = client.execute(request);
+
+            if (response.getStatusLine().getStatusCode() == 200) {
+                JSONObject object = new JSONObject(EntityUtils.toString(response.getEntity()));
+                return object.getLong("activeVotingPower");
+            } else if (response.getStatusLine().getStatusCode() == 400) {
+                JSONObject object = new JSONObject(EntityUtils.toString(response.getEntity()));
+                throw new RuntimeException("Failed with HTTP error 400 and message: " + object.getString("message"));
+            } else {
+                throw new RuntimeException("Failed with HTTP error code : " + response.getStatusLine().getStatusCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 
     /**
      * Queries the RPC node to get the total number of validators (standby & active).
