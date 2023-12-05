32bf6b1d13e51d12b1041b7505b187c7e6a6992f
EAOE3 <haddadedy82@gmail.com>
2023-12-05 16:52:50 +0400

    New Txn

--- src/main/java/com/github/pwrlabs/pwrj/protocol/PWRJ.java
+++ src/main/java/com/github/pwrlabs/pwrj/protocol/PWRJ.java
@@ -561,6 +561,28 @@ public class PWRJ {
         }
     }
 
+    public static long getDelegatedPWR(String delegatorAddress, String validatorAddress) {
+        try {
+            HttpGet request = new HttpGet(rpcNodeUrl + "validator/delegator/delegatedPWROfAddress/?userAddress=" + delegatorAddress + "&validatorAddress=" + validatorAddress);
+            HttpResponse response = client.execute(request);
+
+            if (response.getStatusLine().getStatusCode() == 200) {
+                JSONObject object = new JSONObject(EntityUtils.toString(response.getEntity()));
+
+                return object.getLong("delegatedPWR");
+            } else if (response.getStatusLine().getStatusCode() == 400) {
+                JSONObject object = new JSONObject(EntityUtils.toString(response.getEntity()));
+
+                throw new RuntimeException("Failed with HTTP error 400 and message: " + object.getString("message"));
+            } else {
+                throw new RuntimeException("Failed with HTTP error code : " + response.getStatusLine().getStatusCode());
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 
 
     /**
