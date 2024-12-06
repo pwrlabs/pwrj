d17b305737a8496c95030903d98c1ea849c5baea
eaoe3 <haddadedy82@gmail.com>
2024-12-06 12:23:17 +0400

    - New getBlockWithVmDataTransactionsOnly()

--- src/main/java/com/github/pwrlabs/pwrj/protocol/PWRJ.java
+++ src/main/java/com/github/pwrlabs/pwrj/protocol/PWRJ.java
@@ -357,6 +357,10 @@ public class PWRJ {
         return new Block(httpGet(rpcNodeUrl + "/blockExcludingDataAndExtraData/?blockNumber=" + blockNumber).getJSONObject("block"));
     }
 
+    public Block getBlockWithVmDataTransactionsOnly(long blockNumber, long vmId) throws IOException {
+        return new Block(httpGet(rpcNodeUrl + "/blockWithVmDataTransactionsOnly/?blockNumber=" + blockNumber + "&vmId=" + vmId).getJSONObject("block"));
+    }
+
     public Transaction getTransactionByHash(String hash) throws IOException {
         JSONObject object = httpGet(rpcNodeUrl + "/transactionByHash/?transactionHash=" + hash).getJSONObject("transaction");
         return Transaction.fromJSON(object, object.getLong("blockNumber"), object.getLong("timestamp"), object.getInt("positionInTheBlock"));
