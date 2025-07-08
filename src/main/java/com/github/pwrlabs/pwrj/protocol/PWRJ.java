de1716fdf0b726dce5743dac372817506bdf5c5a
eaoe3 <haddadedy82@gmail.com>
2025-07-08 18:52:23 +0400

    Add timing logs to getBlockAndTransactions for performance monitoring

--- src/main/java/com/github/pwrlabs/pwrj/protocol/PWRJ.java
+++ src/main/java/com/github/pwrlabs/pwrj/protocol/PWRJ.java
@@ -1242,8 +1242,11 @@ public class PWRJ {
     }
 
     public BiResult<Block, List<FalconTransaction>> getBlockAndTransactions(long blockNumber) throws Exception {
+        long startTime = System.currentTimeMillis();
         JSONObject response = httpGet(rpcNodeUrl + "/blockWithTransactions?blockNumber=" + blockNumber);
+        System.out.println("Time taken to get block and transactions: " + (System.currentTimeMillis() - startTime) + "ms");
 
+        startTime = System.currentTimeMillis();
         Block block = new Block(response.getJSONObject("block"));
         JSONArray transactionsArray = response.getJSONArray("transactions");
 
@@ -1253,6 +1256,7 @@ public class PWRJ {
             FalconTransaction transaction = FalconTransaction.fromJson(transactionObject);
             transactions.add(transaction);
         }
+        System.out.println("Time taken to parse transactions: " + (System.currentTimeMillis() - startTime) + "ms");
 
         return new BiResult<>(block, transactions);
     }
